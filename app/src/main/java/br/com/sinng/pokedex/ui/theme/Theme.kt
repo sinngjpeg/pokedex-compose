fd856e8f860b8ff60a4f341ac9feeb047157f9a1
Ingrid Silva <ingridsilvatec@outlook.com>
2024-08-26 18:18:43 -0300

    add initial project

--- app/src/main/java/br/com/sinng/pokedex/ui/theme/Theme.kt
+++ app/src/main/java/br/com/sinng/pokedex/ui/theme/Theme.kt
@@ -1,6 +1,6 @@
 package br.com.sinng.pokedex.ui.theme
 
-import android.app.Activity
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/br/com/sinng/pokedex/ui/theme/Type.kt
+++ app/src/main/java/br/com/sinng/pokedex/ui/theme/Type.kt
@@ -2,9 +2,23 @@ package br.com.sinng.pokedex.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.sinng.pokedex.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
